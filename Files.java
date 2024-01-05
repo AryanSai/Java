88bb9f1b1a4e4fecb91ecf39fcfe52ad066aca1e
Aryan Sai <aryansaiarvapelly@gmail.com>
2024-01-05 14:10:12 +0530

    commit

--- Files.java
+++ Files.java
@@ -1,5 +1,7 @@
 import java.io.File; // Import the File class
 import java.io.IOException; // Import the IOException class to handle errors
+import java.io.FileWriter;
+import java.util.Scanner;
 
 public class Files {
     public static void main(String[] args) {
@@ -14,5 +16,25 @@ public class Files {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+        try {
+            FileWriter fw = new FileWriter("filename2.txt");
+            fw.write("Sairam");
+            fw.close();
+            System.out.println("Success!!");
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        try {
+            File myObj = new File("filename2.txt");
+            Scanner sc = new Scanner(myObj);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+            sc.close();
+            System.out.println("Success Read!!");
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
+
 }
