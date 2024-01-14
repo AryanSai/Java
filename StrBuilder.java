072d1e6c926842a32ee3b7fb3f82c5277d29c3e5
Aryan Sai <aryansaiarvapelly@gmail.com>
2024-01-14 14:57:51 +0530

    string builder

---
+++ StrBuilder.java
@@ -0,0 +1,15 @@
+public class StrBuilder {
+    public static void main(String[] args) {
+        String s = "sairam";
+        StringBuilder sb = new StringBuilder(s);
+        sb.append("sa");
+        System.out.println(sb);
+
+        // Convert StringBuilder to String
+        String resultString = sb.toString();
+        System.out.println(resultString);
+
+        //reverse
+        System.out.println("reverse: " + sb.reverse());
+    }
+}
